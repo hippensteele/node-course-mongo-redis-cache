0bc35582e858da98c6c9a1da94a2e896b7b39de5
Tom <hippensteele@gmail.com>
2018-09-03 19:35:08 +0000

    initial commit

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,6 +3,8 @@ const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
 
+const cleanCache = require('../middlewares/cleanCache');
+
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
@@ -13,9 +15,8 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
-
+  app.get('/api/blogs', requireLogin, cleanCache, async (req, res) => {
+    const blogs = await Blog.find({ _user: req.user.id }).cache({key: req.user.id});
     res.send(blogs);
   });
 
